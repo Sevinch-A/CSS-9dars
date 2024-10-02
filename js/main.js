0e2a97b263af3799e410eadb68c83aaa1ecb9853
Sevinch <sevincha2008@gmail.com>
2024-10-02 22:42:31 +0500

    CSS 9 dars
    Zeromax IT-center 37-group

---
+++ js/main.js
@@ -0,0 +1,286 @@
+let byWrap = document.getElementsByTagName("div").namedItem("wrap");
+let byFormGroup = document.getElementsByTagName("div").namedItem("form-group");
+
+let byLabel1 = document.getElementsByTagName("label").namedItem("label1");
+let byLabel2 = document.getElementsByTagName("label").namedItem("label2");
+let byLabel3 = document.getElementsByTagName("label").namedItem("label3");
+let byLabel4 = document.getElementsByTagName("label").namedItem("label4");
+let byLabel5 = document.getElementsByTagName("label").namedItem("label5");
+let byLabel6 = document.getElementsByTagName("label").namedItem("label6");
+let byLabel7 = document.getElementsByTagName("label").namedItem("label7");
+let byLabel8 = document.getElementsByTagName("label").namedItem("label8");
+let byLabel9 = document.getElementsByTagName("label").namedItem("label9");
+let byLabel10 = document.getElementsByTagName("label").namedItem("label10");
+let byLabel11 = document.getElementsByTagName("label").namedItem("label11");
+let byLabel12 = document.getElementsByTagName("label").namedItem("label12");
+let byLabel13 = document.getElementsByTagName("label").namedItem("label13");
+let byLabel14 = document.getElementsByTagName("label").namedItem("label14");
+let byLabel15 = document.getElementsByTagName("label").namedItem("label15");
+let byLabel16 = document.getElementsByTagName("label").namedItem("label16");
+let byLabel17 = document.getElementsByTagName("label").namedItem("label17");
+let byLabel18 = document.getElementsByTagName("label").namedItem("label18");
+let byLabel19 = document.getElementsByTagName("label").namedItem("label19");
+let byLabel20 = document.getElementsByTagName("label").namedItem("label20");
+
+let byInput1 = document.getElementsByTagName("input").namedItem("firstname");
+let byInput2 = document.getElementsByTagName("input").namedItem("lastname");
+let byInput3 = document.getElementsByTagName("input").namedItem("email");
+let byInput4 = document.getElementsByTagName("input").namedItem("premail");
+let byInput5 = document.getElementsByTagName("input").namedItem("password");
+let byInput6 = document.getElementsByTagName("input").namedItem("color");
+let byInput7 = document.getElementsByTagName("input").namedItem("week");
+let byInput8 = document.getElementsByTagName("input").namedItem("month");
+let byInput9 = document.getElementsByTagName("input").namedItem("date");
+let byInput10 = document.getElementsByTagName("input").namedItem("time");
+
+let bySelect = document.getElementsByTagName("select").namedItem("age")
+let bySelect1 = document.getElementsByTagName("select").namedItem("day")
+let bySelect2 = document.getElementsByTagName("select").namedItem("oy")
+let bySelect3 = document.getElementsByTagName("select").namedItem("year")
+
+
+byWrap.style.width = "1200px";
+byWrap.style.width = "0 auto";
+
+byFormGroup.style.width = "500px";
+byFormGroup.style.margin = "0 auto";
+byFormGroup.style.boxShadow = "0 0 10px rgba(80, 80, 80, 0.5)";
+byFormGroup.style.padding = "1rem 1.5rem";
+byFormGroup.style.boxSizing = "border-box";
+
+byLabel1.style.width = "35%";
+byLabel1.style.padding = "0.5rem 1.5rem";
+byLabel1.style.boxSizing  = "border-box";
+byLabel1.style.display = "inline-block";
+byLabel1.style.textAlign = "right";
+byLabel1.style.marginBottom = "10px";
+
+byInput1.style.width = "60%"
+byInput1.style.padding = "0.5rem 1.5rem"
+byInput1.style.boxSizing = "border-box"
+byInput1.style.border = "1px solid #cccccc"
+byInput1.style.borderRadius = "5px"
+byInput1.style.display = "inline-block"
+byInput1.style.marginBottom = "10px"
+
+byLabel2.style.width = "35%";
+byLabel2.style.padding = "0.5rem 1.5rem";
+byLabel2.style.boxSizing  = "border-box";
+byLabel2.style.display = "inline-block";
+byLabel2.style.textAlign = "right";
+byLabel2.style.marginBottom = "10px";
+
+byInput2.style.width = "60%"
+byInput2.style.padding = "0.5rem 1.5rem"
+byInput2.style.boxSizing = "border-box"
+byInput2.style.border = "1px solid #cccccc"
+byInput2.style.borderRadius = "5px"
+byInput2.style.display = "inline-block"
+byInput2.style.marginBottom = "10px"
+
+byLabel3.style.width = "35%";
+byLabel3.style.padding = "0.5rem 1.5rem";
+byLabel3.style.boxSizing  = "border-box";
+byLabel3.style.display = "inline-block";
+byLabel3.style.textAlign = "right";
+byLabel3.style.marginBottom = "10px";
+
+byInput3.style.width = "60%"
+byInput3.style.padding = "0.5rem 1.5rem"
+byInput3.style.boxSizing = "border-box"
+byInput3.style.border = "1px solid #cccccc"
+byInput3.style.borderRadius = "5px"
+byInput3.style.display = "inline-block"
+byInput3.style.marginBottom = "10px"
+
+byLabel4.style.width = "35%";
+byLabel4.style.padding = "0.5rem 1.5rem";
+byLabel4.style.boxSizing  = "border-box";
+byLabel4.style.display = "inline-block";
+byLabel4.style.textAlign = "right";
+byLabel4.style.marginBottom = "10px";
+
+byInput4.style.width = "60%"
+byInput4.style.padding = "0.5rem 1.5rem"
+byInput4.style.boxSizing = "border-box"
+byInput4.style.border = "1px solid #cccccc"
+byInput4.style.borderRadius = "5px"
+byInput4.style.display = "inline-block"
+byInput4.style.marginBottom = "10px"
+
+byLabel5.style.width = "35%";
+byLabel5.style.padding = "0.5rem 1.5rem";
+byLabel5.style.boxSizing  = "border-box";
+byLabel5.style.display = "inline-block";
+byLabel5.style.textAlign = "right";
+byLabel5.style.marginBottom = "10px";
+
+byInput5.style.width = "60%"
+byInput5.style.padding = "0.5rem 1.5rem"
+byInput5.style.boxSizing = "border-box"
+byInput5.style.border = "1px solid #cccccc"
+byInput5.style.borderRadius = "5px"
+byInput5.style.display = "inline-block"
+byInput5.style.marginBottom = "10px";
+
+byLabel6.style.width = "35%";
+byLabel6.style.padding = "0.5rem 1.5rem";
+byLabel6.style.boxSizing  = "border-box";
+byLabel6.style.display = "inline-block";
+byLabel6.style.textAlign = "right";
+byLabel6.style.marginBottom = "10px";
+
+bySelect.style.width = "20px"
+bySelect.style.display = "inline-block"
+bySelect.style.borderRadius = "5px"
+bySelect.style.border = "1px solid #cccccc"
+bySelect.style.padding = "0.5rem 1.5rem"
+
+bySelect1.style.width = "20px"
+bySelect1.style.display = "inline-block"
+bySelect1.style.borderRadius = "5px"
+bySelect1.style.border = "1px solid #cccccc"
+bySelect1.style.padding = "0.5rem 1.5rem"
+
+bySelect2.style.width = "20px"
+bySelect2.style.display = "inline-block"
+bySelect2.style.borderRadius = "5px"
+bySelect2.style.border = "1px solid #cccccc"
+bySelect2.style.padding = "0.5rem 1.5rem"
+
+bySelect3.style.width = "20px"
+bySelect3.style.display = "inline-block"
+bySelect3.style.borderRadius = "5px"
+bySelect3.style.border = "1px solid #cccccc"
+bySelect3.style.padding = "0.5rem 1.5rem"
+
+byLabel7.style.width = "35%";
+byLabel7.style.padding = "0.5rem 1.5rem";
+byLabel7.style.boxSizing  = "border-box";
+byLabel7.style.display = "inline-block";
+byLabel7.style.textAlign = "right";
+byLabel7.style.marginBottom = "10px";
+
+byInput6.style.width = "60%"
+byInput6.style.padding = "0.5rem 1.5rem"
+byInput6.style.boxSizing = "border-box"
+byInput6.style.border = "1px solid #cccccc"
+byInput6.style.borderRadius = "5px"
+byInput6.style.display = "inline-block"
+byInput6.style.marginBottom = "10px";
+
+byLabel8.style.width = "35%";
+byLabel8.style.padding = "0.5rem 1.5rem";
+byLabel8.style.boxSizing  = "border-box";
+byLabel8.style.display = "inline-block";
+byLabel8.style.textAlign = "right";
+byLabel8.style.marginBottom = "10px";
+
+byInput7.style.width = "60%"
+byInput7.style.padding = "0.5rem 1.5rem"
+byInput7.style.boxSizing = "border-box"
+byInput7.style.border = "1px solid #cccccc"
+byInput7.style.borderRadius = "5px"
+byInput7.style.display = "inline-block"
+byInput7.style.marginBottom = "10px";
+
+byLabel9.style.width = "35%";
+byLabel9.style.padding = "0.5rem 1.5rem";
+byLabel9.style.boxSizing  = "border-box";
+byLabel9.style.display = "inline-block";
+byLabel9.style.textAlign = "right";
+byLabel9.style.marginBottom = "10px";
+
+byInput8.style.width = "60%"
+byInput8.style.padding = "0.5rem 1.5rem"
+byInput8.style.boxSizing = "border-box"
+byInput8.style.border = "1px solid #cccccc"
+byInput8.style.borderRadius = "5px"
+byInput8.style.display = "inline-block"
+byInput8.style.marginBottom = "10px";
+
+byLabel10.style.width = "35%";
+byLabel10.style.padding = "0.5rem 1.5rem";
+byLabel10.style.boxSizing  = "border-box";
+byLabel10.style.display = "inline-block";
+byLabel10.style.textAlign = "right";
+byLabel10.style.marginBottom = "10px";
+
+byInput9.style.width = "60%"
+byInput9.style.padding = "0.5rem 1.5rem"
+byInput9.style.boxSizing = "border-box"
+byInput9.style.border = "1px solid #cccccc"
+byInput9.style.borderRadius = "5px"
+byInput9.style.display = "inline-block"
+byInput9.style.marginBottom = "10px";
+
+byLabel11.style.width = "35%";
+byLabel11.style.padding = "0.5rem 1.5rem";
+byLabel11.style.boxSizing  = "border-box";
+byLabel11.style.display = "inline-block";
+byLabel11.style.textAlign = "right";
+byLabel11.style.marginBottom = "10px";
+
+byInput10.style.width = "60%"
+byInput10.style.padding = "0.5rem 1.5rem"
+byInput10.style.boxSizing = "border-box"
+byInput10.style.border = "1px solid #cccccc"
+byInput10.style.borderRadius = "5px"
+byInput10.style.display = "inline-block"
+byInput10.style.marginBottom = "10px";
+
+byLabel12.style.width = "35%";
+byLabel12.style.padding = "0.5rem 1.5rem";
+byLabel12.style.boxSizing  = "border-box";
+byLabel12.style.display = "inline-block";
+byLabel12.style.textAlign = "right";
+byLabel12.style.marginBottom = "10px";
+
+byLabel13.style.width = "35%";
+byLabel13.style.padding = "0.5rem 1.5rem";
+byLabel13.style.boxSizing  = "border-box";
+byLabel13.style.display = "inline-block";
+byLabel13.style.textAlign = "right";
+byLabel13.style.marginBottom = "10px";
+
+byLabel14.style.width = "35%";
+byLabel14.style.padding = "0.5rem 1.5rem";
+byLabel14.style.boxSizing  = "border-box";
+byLabel14.style.display = "inline-block";
+byLabel14.style.textAlign = "right";
+byLabel14.style.marginBottom = "10px";
+
+byLabel15.style.width = "35%";
+byLabel15.style.padding = "0.5rem 1.5rem";
+byLabel15.style.boxSizing  = "border-box";
+byLabel15.style.display = "inline-block";
+byLabel15.style.textAlign = "right";
+byLabel15.style.marginBottom = "10px";
+
+byLabel16.style.width = "35%";
+byLabel16.style.padding = "0.5rem 1.5rem";
+byLabel16.style.boxSizing  = "border-box";
+byLabel16.style.display = "inline-block";
+byLabel16.style.textAlign = "right";
+byLabel16.style.marginBottom = "10px";
+
+byLabel17.style.width = "35%";
+byLabel17.style.padding = "0.5rem 1.5rem";
+byLabel17.style.boxSizing  = "border-box";
+byLabel17.style.display = "inline-block";
+byLabel17.style.textAlign = "right";
+byLabel17.style.marginBottom = "10px";
+
+byLabel18.style.width = "35%";
+byLabel18.style.padding = "0.5rem 1.5rem";
+byLabel18.style.boxSizing  = "border-box";
+byLabel18.style.display = "inline-block";
+byLabel18.style.textAlign = "right";
+byLabel18.style.marginBottom = "10px";
+
+byLabel19.style.width = "35%";
+byLabel19.style.padding = "0.5rem 1.5rem";
+byLabel19.style.boxSizing  = "border-box";
+byLabel19.style.display = "inline-block";
+byLabel19.style.textAlign = "right";
+byLabel19.style.marginBottom = "10px";
